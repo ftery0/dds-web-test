b13e71d181e290cc584a7259cf6cb370e43186f0
ftery0 <haejun7388@gmail.com>
2026-01-20 14:34:25 +0900

    chore :: TypeScript 설정 및 의존성 업데이트

--- packages/hooks/useQueryInvalidates.ts
+++ packages/hooks/useQueryInvalidates.ts
@@ -1,10 +1,12 @@
-import { useQueryClient } from "react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const useQueryInvalidates = () => {
   const queryClient = useQueryClient();
 
   const queryInvalidates = (queryKeys: (string | (string | number)[])[]) => {
-    return queryKeys.map((item) => queryClient.invalidateQueries(item));
+    return queryKeys.map((item) =>
+      queryClient.invalidateQueries({ queryKey: Array.isArray(item) ? item : [item] })
+    );
   };
 
   return { queryInvalidates };
