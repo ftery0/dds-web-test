358c94132e11ba412ac21606236998a5aab47156
ftery0 <haejun7388@gmail.com>
2026-01-20 14:31:58 +0900

    feat :: package.json에 아이콘 subpath exports 추가

--- packages/assets/index.ts
+++ packages/assets/index.ts
@@ -1,2 +1,4 @@
-export * from "./fonts";
+// Fonts are excluded from bundle to reduce package size
+// Users should install Pretendard font separately or use CDN
+// CDN: https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css
 export * from "./icons";
--- packages/index.ts
+++ packages/index.ts
@@ -1,6 +1,8 @@
-export * from "./assets";
 export * from "./components";
 export * from "./foundations";
 export * from "./hooks";
 export * from "./styles";
 export * from "./utils";
+
+// Icons are excluded from main bundle to reduce package size
+// Import icons separately: import { Bell, School } from 'dds-web-test/icons'
